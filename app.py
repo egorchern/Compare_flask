16121f0e22a90d6ccb100ee98e1c3628f84e7a7c
Egor <Egor@DESKTOP-QGB3P2I>
2020-07-24 22:16:23 +0100

    added myanimelist class and added content-container in template

--- app.py
+++ app.py
@@ -3,15 +3,24 @@ from flask import Flask, render_template;
 import re;
 
 
+class myanimelist:
+    def __init__(self, reviews_list, average_rating, ranking):
+        self.reviews_list = reviews_list;
+        self.average_rating = average_rating;
+        self.ranking = ranking;
 
 
 app = Flask(__name__);
 
 @app.route("/")
 def index():
-    reviews_list = myanimelist_methods.myanimelistReviewDownload("https://myanimelist.net/anime/21/One_Piece");
+    myanimelist_url = myanimelist_methods.myanimelistNameSearch("railgun");
+    reviews_list = myanimelist_methods.myanimelistReviewDownload(myanimelist_url);
+    average_rating, ranking = myanimelist_methods.myanimelistGetRatings(myanimelist_url);
+
+    mal = myanimelist(reviews_list, average_rating, ranking);
 
 
-    return render_template("index.html", reviews_list=reviews_list);
+    return render_template("index.html", mal=mal);
 
 app.run();
